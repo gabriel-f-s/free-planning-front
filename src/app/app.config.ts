4b25c24ecaa17be44d6d21496675022fc3c94c10
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-05-03 17:45:47 -0300

    feat: setup application routing, HTTP client, and PrimeNG framework

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,85 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {AuthInterceptor} from './core/interceptors/auth.interceptor';
+import {providePrimeNG} from 'primeng/config';
+import { DialogService } from 'primeng/dynamicdialog';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import Aura from '@primeng/themes/aura'
+import { definePreset } from '@primeng/themes';
+
+const Noir = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{zinc.50}',
+      100: '{zinc.100}',
+      200: '{zinc.200}',
+      300: '{zinc.300}',
+      400: '{zinc.400}',
+      500: '{zinc.500}',
+      600: '{zinc.600}',
+      700: '{zinc.700}',
+      800: '{zinc.800}',
+      900: '{zinc.900}',
+      950: '{zinc.950}'
+    },
+    colorScheme: {
+      light: {
+        primary: {
+          color: '{zinc.950}',
+          hoverColor: '{zinc.900}',
+          activeColor: '{zinc.800}'
+        },
+        highlight: {
+          background: '{zinc.950}',
+          focusBackground: '{zinc.700}',
+          color: '#ffffff',
+          focusColor: '#ffffff'
+        }
+      },
+      dark: {
+        primary: {
+          color: '{zinc.50}',
+          hoverColor: '{zinc.100}',
+          activeColor: '{zinc.200}'
+        },
+        highlight: {
+          background: 'rgba(250, 250, 250, .16)',
+          focusBackground: 'rgba(250, 250, 250, .24)',
+          color: 'rgba(255,255,255,.87)',
+          focusColor: 'rgba(255,255,255,.87)'
+        }
+      }
+    }
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    provideAnimationsAsync(),
+    DialogService,
+    providePrimeNG({
+      theme: {
+        preset: Noir,
+        options: {
+          darkModeSelector: '.dark'
+        }
+      },
+      zIndex: {
+        modal: 1100,
+        overlay: 9999,
+        menu: 1000,
+        tooltip: 1100
+      }
+    })
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import {Login} from './features/auth/login/login';
+import {Register} from './features/auth/register/register';
+import {Dashboard} from './features/dashboard/dashboard';
+import {authGuard} from './core/guards/auth.guard';
+import {Auth} from './features/auth/auth';
+import {Navbar} from './core/layout/navbar/navbar';
+import {ProjectList} from './features/projects/project-list/project-list';
+import {ProjectDetail} from './features/projects/project-detail/project-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: Auth,
+    children: [
+      { path: 'login', component: Login },
+      { path: 'register', component: Register },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+    ],
+  },
+  {
+    path: '',
+    component: Navbar,
+    canActivate: [authGuard],
+    children: [
+      { path: 'dashboard', component: Dashboard },
+      { path: 'projects', component: ProjectList },
+      { path: 'projects/:id', component: ProjectDetail },
+      { path: '' , redirectTo: 'dashboard', pathMatch: 'full'}
+    ]
+  },
+  { path: '**', redirectTo: 'auth' },
+];
