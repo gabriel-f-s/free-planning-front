84a461ce85e803936a796508ca7dc805955ea984
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-05-13 18:59:00 -0300

    feat(user): add models for user profile, email, and password update requests

--- src/app/core/models/user.model.ts
+++ src/app/core/models/user.model.ts
@@ -5,4 +5,22 @@ export interface User {
   name: string;
   email: string;
   occupation: Occupation;
+  hourlyRate: number;
+}
+
+export interface UserUpdateRequest {
+  name: string;
+  occupation: string;
+  hourlyRate: number;
+}
+
+export interface UserUpdateEmailRequest {
+  email: string;
+  password: string;
+}
+
+export interface UserUpdatePasswordRequest {
+  oldPassword: string;
+  newPassword: string;
+  confirmNewPassword: string;
 }
