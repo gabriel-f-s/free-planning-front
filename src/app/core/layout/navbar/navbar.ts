ee6a291817ca04f9ab568420d319c507ffce9d6a
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-05-13 19:04:29 -0300

    refactor: consolidate navbar component imports and update ui

--- src/app/core/layout/navbar/navbar.ts
+++ src/app/core/layout/navbar/navbar.ts
@@ -1,46 +1,32 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {Router, RouterLink, RouterOutlet} from '@angular/router';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { NgClass } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
-import {AuthService} from '../../services/auth.service';
-import {Dialog} from 'primeng/dialog';
+import { AuthService } from '../../services/auth.service';
+import { Dialog } from 'primeng/dialog';
+import {Toast} from 'primeng/toast';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, NgClass, ButtonModule, Dialog],
+  imports: [RouterOutlet, RouterLink, NgClass, ButtonModule, Dialog, Toast],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar implements OnInit {
+export class Navbar {
   private authService: AuthService = inject(AuthService);
   private router = inject(Router);
 
-  openMenu = false;
   isDarkMode = false;
+
+  openMenu = false;
   showLogoutModal = false;
-
-  ngOnInit(): void {
-    this.isDarkMode = document.documentElement.classList.contains('dark');
-  }
-
-  toggleDarkMode(): void {
-    this.isDarkMode = !this.isDarkMode;
-    const html = document.documentElement;
-    if (this.isDarkMode) {
-      html.classList.add('dark');
-    } else {
-      html.classList.remove('dark');
-    }
-  }
 
   changeMenu(): void {
     this.openMenu = !this.openMenu;
   }
 
-
-
-  logout():void {
+  logout(): void {
     this.showLogoutModal = true;
   }
 
@@ -48,5 +34,22 @@ export class Navbar implements OnInit {
     this.showLogoutModal = false;
     this.authService.removeToken();
     this.router.navigate(['/auth/login']);
+  }
+
+  toggleDarkMode(): void {
+    const newThemeIsDark = !this.isDarkMode;
+    this.applyTheme(newThemeIsDark);
+    localStorage.setItem('freeplaning-theme', newThemeIsDark ? 'dark' : 'light');
+  }
+
+  private applyTheme(isDark: boolean): void {
+    this.isDarkMode = isDark;
+    const html = document.documentElement;
+
+    if (isDark) {
+      html.classList.add('dark');
+    } else {
+      html.classList.remove('dark');
+    }
   }
 }
