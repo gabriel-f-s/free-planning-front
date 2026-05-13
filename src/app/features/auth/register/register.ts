92aa3da1622c64f46d4aea5e1879b9d274b654ad
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-05-13 19:00:09 -0300

    feat(auth): redesign register page UI and update auth service import path

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,16 +1,18 @@
 import {Component, model} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {AuthService} from '../auth.service';
+import {AuthService} from '../../../core/services/auth.service';
 import {Router, RouterLink} from '@angular/router';
 import {AuthResponse, RegisterRequest} from '../../../core/models/auth.model';
 import {Occupation} from '../../../core/enums/occupation.enum';
 import {ButtonDirective} from 'primeng/button';
+import {InputNumber} from 'primeng/inputnumber';
 
 @Component({
   selector: 'app-register',
   imports: [
     ReactiveFormsModule,
     RouterLink,
+    InputNumber,
   ],
   templateUrl: './register.html',
   styleUrl: './register.css',
@@ -22,6 +24,7 @@ export class Register {
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(6)]),
     occupation: new FormControl('', [Validators.required]),
+    hourlyRate: new FormControl('', [Validators.required]),
   })
 
   constructor(private authService: AuthService, private router: Router) {
